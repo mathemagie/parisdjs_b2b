b0c6109d824c294f4ecb3d45514c9dbb43b4671e
mathemagie <aurelien.fache@gmail.com>
2013-05-01 17:04:52 +0200

    choose profil and js in php file

---
+++ www/all_js_footer.php
@@ -0,0 +1,15 @@
+  <script src="http://code.jquery.com/jquery.js"></script>
+    <script src="js/bootstrap-transition.js"></script>
+    <script src="js/bootstrap-alert.js"></script>
+    <script src="js/bootstrap-modal.js"></script>
+    <script src="js/bootstrap-dropdown.js"></script>
+    <script src="js/bootstrap-scrollspy.js"></script>
+    <script src="js/bootstrap-tab.js"></script>
+    <script src="js/bootstrap-tooltip.js"></script>
+    <script src="js/bootstrap-popover.js"></script>
+    <script src="js/bootstrap-button.js"></script>
+    <script src="js/bootstrap-collapse.js"></script>
+    <script src="js/bootstrap-carousel.js"></script>
+    <script src="js/bootstrap-typeahead.js"></script>
+
+    <script src="js/holder/holder.js"></script>
