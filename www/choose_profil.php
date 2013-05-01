63e293e5395df05b8d8ad05297a2d1faafb4926f
mathemagie <aurelien.fache@gmail.com>
2013-05-01 17:31:57 +0200

    small changes

--- www/choose_profil.php
+++ www/choose_profil.php
@@ -1,7 +1,11 @@
 <?php
 	$profil = $_GET['profil'];
 
-	if (isset($profil)) setcookie("profil", $profil, time()+3600);
+	if (isset($profil)) {
+		setcookie("profil", $profil, time()+3600);
+		header('Location: index.php');
+		exit();
+	}
 ?>
 <?php require_once "config.php";?>
 <?php require_once "header.php";?>
@@ -10,13 +14,7 @@
 	<div class="container">
 		<p><a href='choose_profil.php?profil=media' class="btn btn-large btn-primary" type="button">Media</a></p>
 		<p><a href='choose_profil.php?profil=revendeur' class="btn btn-large btn-primary" type="button">Revendeur</a></p>
-		
  	</div> <!-- /container -->
-
-    <!-- Le javascript
-    ================================================== -->
-    <!-- Placed at the end of the document so the pages load faster -->
- 
 	<?php require_once "all_js_footer.php";?>
 	 </body>
 </html>
--- www/press.php
+++ www/press.php
@@ -1,47 +1,44 @@
 <?php require_once "header.php";?>
-								   
-  <body>
+<body>
 <?php require_once "menu.php";?>
 
     <div class="container">
 
-      <!-- Main hero unit for a primary marketing message or call to action -->
     
     
       <h1>Press</h1>
 		<p>Find here the press releases, media and information about our projects</p>
 
-		<ul class="thumbnails">
-  <li class="span3">
-    <div class="thumbnail">
-      <img class="img-polaroid" src="img/Grant_Phabao_presents_The_Lone_Ranger_and_Echo_Minott-Plant_Up_A_Vineyard.jpg" alt=""/>
-      <h3>Titre press release</h3>
-    
-    </div>
-  </li>
+	<ul class="thumbnails">
+    <li class="span3">
+      <div class="thumbnail">
+        <img class="img-polaroid" src="img/Grant_Phabao_presents_The_Lone_Ranger_and_Echo_Minott-Plant_Up_A_Vineyard.jpg" alt=""/>
+        <h3>Titre press release</h3>
+      
+      </div>
+    </li>
    <li class="span3">
     <div class="thumbnail">
       <img class="img-polaroid" src="img/Grant_Phabao_presents_The_Lone_Ranger_and_Echo_Minott-Plant_Up_A_Vineyard.jpg" alt=""/>
       <h3>Titre press release</h3>
   
     </div>
-  </li>
-  <li class="span3">
-    <div class="thumbnail">
-    <img class="img-polaroid" src="img/Grant_Phabao_presents_The_Lone_Ranger_and_Echo_Minott-Plant_Up_A_Vineyard.jpg" alt=""/>
-      <h3>Titre press release</h3>
-     
-    </div>
-  </li>
-  <li class="span3">
-    <div class="thumbnail">
-       <img class="img-polaroid" src="img/Grant_Phabao_presents_The_Lone_Ranger_and_Echo_Minott-Plant_Up_A_Vineyard.jpg" alt=""/>
-      <h3>Titre press release</h3>
+    </li>
+    <li class="span3">
+      <div class="thumbnail">
+      <img class="img-polaroid" src="img/Grant_Phabao_presents_The_Lone_Ranger_and_Echo_Minott-Plant_Up_A_Vineyard.jpg" alt=""/>
+        <h3>Titre press release</h3>
+       
+      </div>
+    </li>
+    <li class="span3">
+      <div class="thumbnail">
+         <img class="img-polaroid" src="img/Grant_Phabao_presents_The_Lone_Ranger_and_Echo_Minott-Plant_Up_A_Vineyard.jpg" alt=""/>
+        <h3>Titre press release</h3>
 
-    </div>
-  </li>
- 
-</ul>
+      </div>
+    </li>
+  </ul>
 		
       <!-- Example row of columns -->
      <!-- <div class="row">
@@ -82,24 +79,6 @@
 
     </div> <!-- /container -->
 
-    <!-- Le javascript
-    ================================================== -->
-    <!-- Placed at the end of the document so the pages load faster -->
-   <script src="http://code.jquery.com/jquery.js"></script>
-    <script src="js/bootstrap-transition.js"></script>
-    <script src="js/bootstrap-alert.js"></script>
-    <script src="js/bootstrap-modal.js"></script>
-    <script src="js/bootstrap-dropdown.js"></script>
-    <script src="js/bootstrap-scrollspy.js"></script>
-    <script src="js/bootstrap-tab.js"></script>
-    <script src="js/bootstrap-tooltip.js"></script>
-    <script src="js/bootstrap-popover.js"></script>
-    <script src="js/bootstrap-button.js"></script>
-    <script src="js/bootstrap-collapse.js"></script>
-    <script src="js/bootstrap-carousel.js"></script>
-    <script src="js/bootstrap-typeahead.js"></script>
-
-    <script src="js/holder/holder.js"></script>
-	
+  <?php require_once "all_js_footer.php";?>
   </body>
 </html>
