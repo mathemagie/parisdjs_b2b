fbe809b238ad539e19f30947a40b9b7e2884ef2b
Laurent Poinsignon <poinsignon@gmail.com>
2013-05-05 01:47:37 +0200

    same footer in php, all pages

---
+++ www/footer.php
@@ -0,0 +1,9 @@
+      <hr>
+      <footer id="footer">
+		<div class="row-fluid">
+			<div class="span3">Mailing list</div>
+			<div class="span3">Help & FAQ</div>
+			<div class="span3"><a href="contact.php">Contact</a></div>
+			<div class="span3">&copy; ParisDjs 2013</div>
+		</div>
+      </footer>
