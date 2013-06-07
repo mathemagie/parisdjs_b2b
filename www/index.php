44a71d44032948596c4ec6c1d618303099908fb9
Laurent Poinsignon <poinsignon@gmail.com>
2013-06-07 23:59:34 +0200

    3 blocks format out of 5
    1-Catalog
    2-Preorder
    3-Stock

--- www/index.php
+++ www/index.php
@@ -1,4 +1,4 @@
-<?php require_once "config.php";?>
+﻿<?php require_once "config.php";?>
 <?php require_once "header.php";?>
   <body>
   <?php require_once "menu.php";?>
@@ -22,48 +22,59 @@
 
 				<div class="item">
 					<div class="picture">
-						<a class="image" href="press_single.php" title="More details"><img src="img/Grant_Phabao_presents_The_Lone_Ranger_and_Echo_Minott-Plant_Up_A_Vineyard.jpg" alt=""></a>
+						<a class="image" href="press_single.php" title="More details"><img src="img/ggp.jpg" alt=""></a>
 						<div class="description">
-							<h2>Element title</h2>
+							<p class="artist">Gregsky & Grant Phabao</p>
+							<h2>La danse du shadow</h2>
+							<p>Paris DJs / Noble Art 001</p>
 							<p>
 								Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.
 							</p>
-						</div>
-						<div class="meta">
-							<span><i class="fa-icon-calendar"></i>4 May 2013</span>
-							<span><i class="fa-icon-heart-empty"></i> 88</span>
+							<p class="meta">04/05/2013</p>
+							<a href="#" class="btn btn-warning maj">Download</a>
 						</div>
 					</div>
 				</div>
 
 				<div class="item">
 					<div class="picture">
-						<a class="image" href="press_single.php" title="More details"><img src="img/Grant_Phabao_presents_The_Lone_Ranger_and_Echo_Minott-Plant_Up_A_Vineyard.jpg" alt=""></a>
+						<a class="image" href="press_single.php" title="More details"><img src="img/good-for-you.jpg" alt=""></a>
 						<div class="description">
-							<h2>Element long title that goes up to 2 lines</h2>							
+							<p class="artist">Paris DJs Soundsystem</p>
+							<h2 class="maj">Dis is good for you</h2>							
+							<p>Paris DJs 008</p>
 							<p>
-								Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.
+								Afrofunk/Tropical/Reggae <br/>
+								2LP at 500 copies only <span class="pull-right">12.50€</span></p>
+							
+							<p>Progress status: <span class="pull-right strong">80%</span>
+								<div class="progress">
+									<div class="bar" style="width: 80%;"></div>
+								</div>
+								20 units left before pressing
 							</p>
-						</div>
-						<div class="meta">
-							<span><i class="fa-icon-calendar"></i>4 May 2013</span>
-							<span><i class="fa-icon-heart-empty"></i> 88</span>
+							<a href="#" class="btn btn-warning maj">Pre-order</a>
 						</div>
 					</div>
 				</div>
 
 				<div class="item">
 					<div class="picture">
-						<a class="image" href="press_single.php" title="More details"><img src="img/Grant_Phabao_presents_The_Lone_Ranger_and_Echo_Minott-Plant_Up_A_Vineyard.jpg" alt=""></a>
+						<a class="image" href="press_single.php" title="More details"><img src="img/lone-ranger.jpg" alt=""></a>
 						<div class="description">
-							<h2>Element title</h2>
-							<p>
-								Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.
+							<p class="artist">Grant Phabao & The Lone Ranger</p>
+							<h2 class="maj">Fever / Version</h2>
+							<p>Paris DJs / T.I.M.E.C. 45 01
+							<br/>Reggae/Dub 7"
+							<br/>300 copies only <span class="pull-right">5.00€</span>
 							</p>
-						</div>
-						<div class="meta">
-							<span><i class="fa-icon-calendar"></i>4 May 2013</span>
-							<span><i class="fa-icon-heart-empty"></i> 88</span>
+							<p>Stock level: <span class="pull-right strong">60%</span>
+								<div class="progress">
+									<div class="bar" style="width: 60%;"></div>
+								</div>
+								120 units left in stock
+							</p>
+							<a href="#" class="btn btn-warning maj">Order</a>
 						</div>
 					</div>
 				</div>
